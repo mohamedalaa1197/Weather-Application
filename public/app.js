24fdaae370fe4015714c3d428e5c360f5d911883
mohamedalaa1197 <62913078+mohamedalaa1197@users.noreply.github.com>
2021-06-02 18:08:13 +0200

    last

--- public/app.js
+++ public/app.js
@@ -2,12 +2,14 @@ const weatherForm = document.querySelector("form")
 const searchElement = document.querySelector("input");
 const message01 = document.querySelector("#message-01");
 const message02 = document.querySelector("#message-02");
+const message03 = document.querySelector("#message-03");
 
 weatherForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     message01.textContent = "loading....";
     message02.textContent = " ";
+    message03.textContent = " ";
 
     fetch("/weather?location=" + searchElement.value)
         .then((response) => {
@@ -17,10 +19,11 @@ weatherForm.addEventListener("submit", (e) => {
                 if (data.error) {
                     message01.textContent = data.error;
                 } else {
-
                     message01.textContent = "Location is: " + data.Place_name;
 
-                    message02.textContent = "Temperature is: " + data.Temperature.Temperature;
+                    message02.textContent = "Temperature is: " + data.Temperature;
+
+                    message03.textContent = "Humidity is: " + data.Humidity;
                 }
             })
         });
